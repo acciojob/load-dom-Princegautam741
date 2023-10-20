94154da73784c8d5cc92f1c65505222d0d852f02
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-20 14:07:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+// Add an event listener to detect when the DOM is loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Create a new paragraph element
+    var paragraph = document.createElement("p");
+    
+    // Set the text content of the paragraph
+    paragraph.textContent = "DOM load success";
+    
+    // Append the paragraph to the body of the page
+    document.body.appendChild(paragraph);
+});
